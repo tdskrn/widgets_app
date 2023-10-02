7e2b056f590e55fb9f972230c15abb9797ed3443
Vinicius S. Rocha <viniciusbbbjj@gmail.com>
2023-10-02 20:10:49 -0300

    screens creadas e barril atualizado

---
+++ lib/presentation/screens/animated/animated_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AnimatedScreen extends StatelessWidget {
+  static const name = 'animated-screen';
+  const AnimatedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppTutorialScreen extends StatelessWidget {
+  static const name = 'app-tutorial-screen';
+  const AppTutorialScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
